320bff1cc21c17cb311e118a608864b2e26b3ccc
kivancaksoy <kvnc96@hotmail.com>
2022-04-06 14:20:57 +0300

    ClassMetotDemo Eklendi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri eklendi : " + musteri.Ad + " " + musteri.SoyAd);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi : " + musteri.Ad + " " + musteri.SoyAd);
+        }
+
+        public void ListAll(params Musteri[] musteriler)
+        {
+            Console.WriteLine("Musteriler Listelendi.");
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + " " + musteri.Ad + " " + musteri.SoyAd);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Kıvanç";
+            musteri1.SoyAd = "Aksoy";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Mert";
+            musteri2.SoyAd = "Taşkent";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Mustafa";
+            musteri3.SoyAd = "Demir";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.ListAll(musteri1, musteri2, musteri3);
+        }
+    }
+}
