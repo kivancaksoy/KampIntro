d8735b4313146769ef5095e9bdd020de2a1888c1
kivancaksoy <kvnc96@hotmail.com>
2022-04-19 22:35:44 +0300

    GameProject added.

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    internal class GamerManager : IGamerService
+    {
+        //MicroService (GamerManager'ın içinde bambaşka bir servis kullanmak. Ör:  IGamerService, IUserValidationService)
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer))
+            {
+                Console.WriteLine("Kayıt oldu.");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama başarısız. Kayıt başarısız.");
+            }
+            
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt silindi.");
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt güncellendi.");
+        }
+    }
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer { Id = 1, BirthYear = 1985, FirstName = "KIVANÇ", LastName = "AKSOY", IdentityNumber = 12345 });
+        }
+    }
+}
